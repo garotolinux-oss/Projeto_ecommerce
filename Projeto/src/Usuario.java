b84277920152ef50e65038a69274599a776867e9
GarotoLinux <garotolinux@gmail.com>
2026-05-29 17:59:53 -0300

    criação da classe usuario

---
+++ Projeto/src/Usuario.java
@@ -0,0 +1,31 @@
+public abstract class Usuario {
+    //atributos privados
+    private String nome;
+    private String senha;
+    //Construtor -- os parametros são obrigatorios, afinal se faz necessário nome e senha para ser usuario
+    public Usuario(String nome, String senha) {
+        this.nome = nome;
+        this.senha = senha;
+    }
+    //getters and setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+
+    
+
+}
