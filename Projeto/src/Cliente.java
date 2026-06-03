6d530fd1d5131d2ac72f680f3f3b32127535cf27
GarotoLinux <garotolinux@gmail.com>
2026-06-02 23:47:39 -0300

    cliente

---
+++ Projeto/src/Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente extends Usuario{
+    //atributos
+    
+    public Cliente(String nome, String senha) {
+        super(nome, senha);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
